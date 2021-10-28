c6cb9fd8a7bcef6b5ea50b0b49cc140d03202fe2
nicolashall8 <nicolas.hall@mymail.champlain.edu>
2021-10-27 17:42:29 -0700

    Added export_logs_script.ps1 and send_email_script.ps1

---
+++ Week 8 Homework/export_logs_script.ps1
@@ -0,0 +1,18 @@
+﻿# Storyline: Review the Security Event Log
+
+# Directory to save files:
+$myDir = "C:\Users\champuser\Documents"
+
+# List all the available Windows Event Logs
+Get-EventLog -list
+
+# Create a prompt to allow user to select the Log to view
+# Read-host takes input from the user
+$readLog = Read-host -Prompt "Please select a log to review from the list above."
+
+# Create a prompt to specify what keyword in the log to search for
+$messageLog = Read-host -Prompt "Please write a keyword to search for in the logs."
+
+# Print the results for the log
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike "*$messageLog*" } | export-csv -NoTypeInformation `
+-Path "$myDir\securityLogs.csv"
---
+++ Week 8 Homework/send_email_script.ps1
@@ -0,0 +1,20 @@
+﻿# Storyline: Send an email.
+
+# Variable can have an underscore or any alphanumeric value.
+# $ signifies creation of a variable
+
+
+# Body of the email
+$msg = "Hello there."
+
+# Write-host prints msg variable to the screen
+write-host -BackgroundColor Red -ForegroundColor white $msg
+
+# Email From Address
+$email = "nicolas.hall@mymail.champlain.edu"
+
+# To address
+$toEmail = "deployer@csi-web"
+
+# Sending the email
+Send-MailMessage -From $email -To $toEmail -Subject "A Greeting" -Body $msg -SmtpServer 192.168.6.71
